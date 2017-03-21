300f4849a5bc028f6d75865e2f5884adeb617110
shitijavlani <shitij.avlani@gmail.com>
2017-03-21 19:41:35 +0530

    First Commit

---
+++ PWM Example/PWM_Init.h
@@ -0,0 +1,17 @@
+/*
+ * PWM_Init.h
+ *
+ *  Created on: Mar 21, 2017
+ *      Author: Shitij Avlani
+ */
+
+#ifndef PWM_INIT_H_
+#define PWM_INIT_H_
+
+
+// Prototypes of functions exported.
+extern void PWMLedConfig (void);
+extern void SetPowerLEDDutyCycle (int16_t i16NewDutyCycle);
+
+
+#endif /* PWM_INIT_H_ */
